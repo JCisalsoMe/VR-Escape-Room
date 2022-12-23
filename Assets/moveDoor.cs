9d2b1f1809aae4af38ee50d822efcefdef7f8f7e
JC <08013152@uhi.ac.uk>
2022-12-23 10:34:11 +0000

    Move Door added

--- Assets/moveDoor.cs
+++ Assets/moveDoor.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class moveDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public GameObject[] Star_Door;
+
+    private bool keyAdded;
+
+    private bool unlocked = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (moveDoor() == true && unlocked == false)
+        {
+            Debug.Log(moveDoor());
+
+            gameObject.AddComponent<ConstantForce>().force = new Vector3(1500f,0f,0f);
+            unlocked = true;
+        }
+    }
+
+    private bool moveDoor () {
+        for (int i = 0; i < Star_Door.length; i++)
+        {
+            partAdded = Star_Door[i].GetComponent<SnapObject>().isSnapped;
+            if (partAdded == false)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
